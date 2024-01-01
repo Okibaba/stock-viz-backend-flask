eb86c9236858ecdcc15982b053b1d80d9629c2a5
Okibaba <abiodunoki@gmail.com>
2023-12-31 23:25:07 -0700

    clean sql codes

--- flask/stock_viz_app/init_db.sql
+++ flask/stock_viz_app/init_db.sql
@@ -1,35 +1,25 @@
 CREATE DATABASE "stock-viz";
 
 
--- -- Database configuration
--- SET statement_timeout = 0;
--- SET lock_timeout = 0;
--- SET client_encoding = 'UTF8';
--- SET standard_conforming_strings = on;
--- SET check_function_bodies = false;
--- SET client_min_messages = warning;
--- SET default_tablespace = '';
--- SET default_with_oids = false;
-
 -- -- CREATE tables
 
--- CREATE TABLE stocks (
---     id SERIAL,    
---     stock_symbol TEXT UNIQUE NOT NULL,
---     company_name TEXT UNIQUE NOT NULL,
---     company_description TEXT,
---     PRIMARY KEY (id)
--- );
+CREATE TABLE stocks (
+    id SERIAL,    
+    stock_symbol TEXT UNIQUE NOT NULL,
+    company_name TEXT UNIQUE NOT NULL,
+    company_description TEXT,
+    PRIMARY KEY (id)
+);
 
--- CREATE TABLE prices (
---     id SERIAL PRIMARY KEY,
---     time_stamp TIMESTAMP NOT NULL,
---     current_price NUMERIC,
---     stock_id INT NOT NULL   
--- );
+CREATE TABLE prices (
+    id SERIAL PRIMARY KEY,
+    time_stamp TIMESTAMP NOT NULL,
+    current_price NUMERIC,
+    stock_id INT NOT NULL   
+);
 
 -- -- Add foreign key constraints
--- ALTER TABLE prices
--- ADD CONSTRAINT fk_prices_stocks
--- FOREIGN KEY (stock_id) 
--- REFERENCES stocks (id);
+ALTER TABLE prices
+ADD CONSTRAINT fk_prices_stocks
+FOREIGN KEY (stock_id) 
+REFERENCES stocks (id);
